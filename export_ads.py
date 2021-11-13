984fbb00b2eedbb95cff7c2a39d03cc456e2dad6
le3mon <okko2929@gmail.com>
2021-11-13 21:31:39 +0900

    Add Code

---
+++ export_ads.py
@@ -0,0 +1,23 @@
+import json
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage : export_ads.py [source file] [export file]")
+    sys.exit()
+
+file_path = sys.argv[1]
+export = sys.argv[2]
+
+jlist = []
+ads = []
+for line in open(file_path, 'r'):
+    jlist.append(json.loads(line))
+
+for i in range(len(jlist)):    
+    for j in range(len(jlist[i]["FilesLoaded"].split(', '))):
+        if(jlist[i]["FilesLoaded"].split(', ')[j].rfind(':') != -1):
+            ads.append({"ads": jlist[i]["FilesLoaded"].split(', ')[j], "SourceFileName": jlist[i]["SourceFilename"], "ExecutableName":jlist[i]["ExecutableName"]})
+
+
+with open(export, 'w') as f:
+    json.dump(ads, f, indent=2, ensure_ascii=False)
