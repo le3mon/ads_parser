7c2d1755835f808b6728da4c73d4b8a60d88609a
le3mon <okko2929@gmail.com>
2022-01-10 16:58:18 +0900

    Add kind of data

--- export_ads.py
+++ export_ads.py
@@ -13,11 +13,15 @@ ads = []
 for line in open(file_path, 'r'):
     jlist.append(json.loads(line))
 
-for i in range(len(jlist)):    
-    for j in range(len(jlist[i]["FilesLoaded"].split(', '))):
-        if(jlist[i]["FilesLoaded"].split(', ')[j].rfind(':') != -1):
-            ads.append({"ads": jlist[i]["FilesLoaded"].split(', ')[j], "SourceFileName": jlist[i]["SourceFilename"], "ExecutableName":jlist[i]["ExecutableName"]})
-
+for i in range(len(jlist)):
+    for j in range(len(jlist[i]["FilesLoaded"].split(','))):
+        if(jlist[i]["FilesLoaded"].split(',')[j].find(':') != -1):
+            ads.append({"ads": jlist[i]["FilesLoaded"].split(', ')[j]
+            , "SourceFileName": jlist[i]["SourceFilename"]
+            , "ExecutableName":jlist[i]["ExecutableName"]
+            , "size": jlist[i]["Size"]
+            , "count":jlist[i]["RunCount"]
+            , "last run":jlist[i]["LastRun"]})
 
 with open(export, 'w') as f:
     json.dump(ads, f, indent=2, ensure_ascii=False)
